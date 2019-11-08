c268e05d3aa330f0fa369da898c2573f8cf20f16
toozig <toozig@gmail.com>
2019-11-08 12:14:20 +0200

    add all ex files

--- ex1.py
+++ ex1.py
@@ -3,10 +3,24 @@
 
 
 ## import
-from Bio import SeqIO
+from biopython import SeqIO
 
 ##constance
 
 
-def
-ss
+def readfasta(fastaFile):
+    """
+    Reads sequence from fasta file
+    :param fastaFile: the sequnce file location & name  as a fasta file
+    :return: a string of the given sequence
+    """
+    sequence = SeqIO.parse(fastaFile, "fasta")
+    a = 3
+    return sequence
+
+
+
+
+
+if __name__ == '__main__':
+    readfasta("/cs/usr/toozig/year3/Cbio/cBIOEX1/fastas/HelicoverpaArmigera-cMyc.fasta")
