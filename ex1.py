55266728d23db45508a4ca68a8991f3230a6206c
toozig <toozig@gmail.com>
2019-11-08 16:16:26 +0200

    doing good. problem with innit

--- ex1.py
+++ ex1.py
@@ -7,7 +7,66 @@ from Bio import SeqIO
 import numpy as np
 
 ##constance
+ORIGIN = 1
+SCORE = 0
+SCORE_AND_ORIGIN = 2
 GAP = '-'
+GET_SCORE = lambda x, y: scoring_dict[ord(x) + ord(y)]
+
+
+class Alignment:
+    """
+     Holds the information for each cell in the alignment matrix
+     """
+
+    def __init__(self, mother=None, index=None, score=0):
+        """
+        Ctor for the Class
+        """
+
+        self.__mother = None
+        self.__index = None
+        self.__score = 0
+
+    def set_mother(self, mother):
+        """
+        sets the origin to know the alignment for printing
+        """
+        self.__mother = mother
+
+    def set_index(self, pair):
+        """
+        sets the base pairing of this aligment
+        """
+        self.__index = pair
+
+    def set_score(self, score):
+        """
+        sets the score for this aligment
+        """
+        self.__score = score
+
+    def get_mother(self):
+        """
+        gets the origin to know the alignment for printing
+        """
+        return self.__score
+
+    def get_index(self):
+        """
+        gets the base pairing of this aligment
+        """
+        return self.__index
+
+    def get_score(self):
+        """
+        gets the score for this aligment
+        """
+        return self.__score
+
+    def __repr__(self):
+        return str(self.__score)
+
 
 def readfasta(fastaFile):
     """
@@ -25,7 +84,7 @@ def createScoringDict(file):
     for i in range(len(bases)):
         for j in range(len(bases)):
             val = ord(bases[i]) + ord(bases[j])
-            scoring_dict[val] = matrix[i][j]
+            scoring_dict[val] = int(matrix[i][j])
     return scoring_dict
 
 
@@ -33,37 +92,63 @@ def createScoringDict(file):
 
 def globalScore(seq_one, seq_two, scoring_dict):
     """
-    This function evalute the score of two sequences
+    This function evalute the score of two sequencestype=(int, SCORE_AND_ORIGIN)
     :param seq_one: string of sequence
     :param seq_two: string of sequence
     :param scoring_dict: dictionary with the given scores
     :return: the score of the best alligment
     """
-    matrix = init_matrix(seq_one, seq_two)
+    matrix = init_matrix(seq_one, seq_two, scoring_dict)
+    for i in range(1, len(seq_one) + 1):
+        for j in range(1, len(seq_two) + 1):
+            matrix[i][j].set_index = (i, j)
+            alignment_tup = get_best_alligment(matrix, (i, j), scoring_dict, seq_one, seq_two)
+            matrix[i][j].set_score(alignment_tup[SCORE])
+            matrix[i][j].set_mother(alignment_tup[ORIGIN])
+
+    print(matrix)
 
 
 
-
-
-def init_matrix(seq_one, seq_two):
+def init_matrix(seq_one, seq_two, scoring_dict):
     """
     creates the matrix for the global score algorithim
     """
     # todo put it in a tuple so we know its the begining
-    matrix = np.empty((len(seq_one) + 1, len(seq_two) + 1))
-    # initilaize matrix
-    get_score = lambda x, y: scoring_dict[ord(x) + ord(y)]
-    matrix[0][0] = 0
+    matrix = [[Alignment() for i in range(len(seq_two) + 1)]
+              for j in range((len(seq_one) + 1))]
+        # np.empty((len(seq_two) + 1, len(seq_one) + 1), dtype=Alignment())
+    #initilaize matrix
     for i in range(1, len(matrix[0])):
-        gap_score = get_score(GAP, seq_one[i - 1])
-        matrix[0][i] = matrix[0][i - 1] + gap_score
+        gap_score = GET_SCORE(GAP, seq_one[i - 1])
+        matrix[0][i].set_score(matrix[0][i - 1].get_score() + gap_score)
     for i in range(1, len(seq_two) + 1):
-        gap_score = get_score(seq_two[i - 1], GAP)
-        matrix[i][0] = matrix[i - 1][0] + gap_score
+        gap_score = GET_SCORE(seq_two[i - 1], GAP)
+        matrix[i][0].set_score(matrix[i - 1][0].get_score() + gap_score)
     return matrix
+
+def get_best_alligment(matrix, cur_idx, scoring_dict, seq_one, seq_two):
+    """
+    evaluates the value for this index best alligment
+    """
+    cur_score = GET_SCORE(seq_one[cur_idx[0] - 1], seq_two[cur_idx[1] - 1])
+    options_dict = {matrix[cur_idx[0] - 1][cur_idx[1] - 1].get_score() + cur_score:
+                        (cur_idx[0] - 1, cur_idx[1] - 1),
+                    matrix[cur_idx[0] - 1][cur_idx[1]].get_score() + cur_score:
+                        (cur_idx[0] - 1, cur_idx[1]),
+                    matrix[cur_idx[0]][cur_idx[1] - 1].get_score() + cur_score:
+                        (cur_idx[0], cur_idx[1] - 1)}
+    max_score = max(options_dict)
+
+    return max_score, options_dict[max_score]
+
+
 
 
 if __name__ == '__main__':
-    scoring_dict = createScoringDict("C:\\Users\\133\\Documents\\school\\year3\\semester a\\CBIO\\ex1\\score_matrix.tsv")
-    readfasta("/cs/usr/toozig/year3/Cbio/cBIOEX1/fastas/HelicoverpaArmigera-cMyc.fasta")
+    scoring_dict = createScoringDict("/cs/usr/toozig/year3/Cbio/cBIOEX1/score_matrix.tsv")
+    # readfasta("/cs/usr/toozig/year3/Cbio/cBIOEX1/fastas/HelicoverpaArmigera-cMyc.fasta")
+    a = 'AGCT'
+    b = 'GCT'
+    globalScore(a,b, scoring_dict)
 
